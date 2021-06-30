d018980d3c5d6207f1f02cfc914930008d967855
morteza-sedghian <m.sedqian@gmail.com>
2021-06-30 06:27:04 +0430

    Adding my java progrms to git for the first time

---
+++ Second.java
@@ -0,0 +1,8 @@
+public class Second {
+	public static void main(String[] args) {
+		System.out.println(3.14);
+		System.out.println(3.14 * 2 + 12);
+		System.out.println("Hello");
+		System.out.println(3_000_000);
+	}
+}
